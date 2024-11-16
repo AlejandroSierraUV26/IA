e0646c503162e05cfc42f984bf4c45899e1fdd53
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2024-11-15 23:12:40 -0500

    Codigo en svg

--- combinacion.py
+++ combinacion.py
@@ -32,33 +32,23 @@ def imprimir_arbol(arbol):
     for nodo in arbol:
         print(nodo, arbol[nodo])
 def dibujar_arbol(arbol, camino):
-    G = nx.Graph()  # Crear un grafo vacío
-
-    # Agregar nodos y aristas al grafo
     for nodo in arbol:
-        for hijo, direccion in arbol[nodo]:
-            G.add_edge(nodo, hijo, label=str(direccion))  # Agregar las aristas con las etiquetas
-
-    # Colorear los nodos del camino recorrido de rojo
-    color_map = []
-    for nodo in G.nodes():
         if nodo in camino:
-            color_map.append('red')  # Nodo del camino en rojo
+            g.node(str(nodo), color='red', style='filled')
         else:
-            color_map.append('skyblue')  # Resto de los nodos en azul claro
-
-    # Dibujo de la red
-    pos = nx.spring_layout(G, seed=42)  # Layout para los nodos
-    plt.figure(figsize=(12, 12))
-    nx.draw(G, pos, with_labels=True, node_color=color_map, node_size=500, font_size=12, font_weight='bold', edge_color='gray')
-
-    # Mostrar las etiquetas de las aristas (direcciones)
-    edge_labels = nx.get_edge_attributes(G, 'label')
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-
-    plt.title("Árbol de Caminos")
-    plt.show()
-
+            g.node(str(nodo))
+        for hijo, direccion in arbol[nodo]:
+            if direccion == (-1, 0):
+                direccion = 'Arriba'
+            elif direccion == (1, 0):
+                direccion = 'Abajo'
+            elif direccion == (0, -1):
+                direccion = 'Izquierda'
+            elif direccion == (0, 1):
+                direccion = 'Derecha'
+            
+            g.edge(str(nodo), str(hijo), label=str(direccion))
+    g.view()
 def main():
     mapa = np.array([
     [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -292,5 +282,5 @@ def leer_camino_recorrido():
             
     return elem
 
-
+g = Graph('G', filename='arbol', format='svg')   
 main()
